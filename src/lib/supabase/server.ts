3ab493d3bb7c8a1fe1e6a555df0076b017998c81
İsa Özçıplak <isaozciplak918@gmail.com>
2026-04-08 22:01:42 +0300

    Supabase cookie tip hatası giderildi

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -12,7 +12,7 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
@@ -37,12 +37,12 @@ export async function createAdminClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
             );
-          } catch {}
+          } catch { }
         },
       },
     }
